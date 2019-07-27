8bb84a72ac41ca5322506f2d54d6d0b711337e27
cavb28 <cavb.28@gmail.com>
2019-07-27 19:09:34 +0000

    Added header file

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function putchar prototype
+ */
+int _printf(const char *format, ...);
+#endif
+
