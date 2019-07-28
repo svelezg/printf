c4f74a1a6ef5bb3c2cbf25af0c8a9406c4f2d1e2
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-28 21:42:53 +0000

    Updated header file new str_prinf.c file

--- holberton.h
+++ holberton.h
@@ -7,5 +7,49 @@
  * Function putchar prototype
  */
 int _printf(const char *format, ...);
+
+char *str_printf(const char *format, va_list valist, char *str);
+int size_printf(const char *const format, va_list valist);
+
+int size_char(va_list valist, int size);
+int size_string(va_list valist, int size);
+int size_decimal(va_list valist, int size);
+int size_integer(va_list valist, int size);
+int size_percentage(int size);
+
+int char_printf(va_list valist, char *str);
+int string_printf(va_list valist, char *str);
+int decimal_printf(va_list valist, char *str);
+int integer_printf(va_list valist, char *str);
+int percentage_printf(char *str);
+
+/**
+ * struct op_size - Struct op
+ *
+ * @ops: options c: char, i:integer, f:float, s: char
+ * @f: function
+ *
+ */
+typedef struct op_size
+{
+	const char *const ops;
+	int (*f)(va_list valist, int size);
+} op_s_t;
+
+
+
+/**
+ * struct op_write - Struct op
+ *
+ * @opw: options c: char, i:integer, f:float, s: char
+ * @f: function
+ *
+ */
+typedef struct op_write
+{
+	const char *const opw;
+	int (*f)(va_list valist, char *str);
+} op_w_t;
+
 #endif
 
---
+++ str_printf.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "holberton.h"
+
+/**
+ * str_printf - Replicates the c standar library printf() function
+ * @format: The operator
+ * @valist: The operator
+ * @str: The operator
+ *
+ * Return: number of  characters printed
+ */
+char *str_printf(const char *format, va_list valist, char *str)
+{
+int index = 0;
+int index_struct = 0, position = 0;
+op_w_t func_write[] = {
+	{"c", char_printf},
+{	"s", string_printf},
+	{NULL, NULL}
+};
+while (*(format + index))
+{
+	if (*(format + index) == '%')
+	{
+		index_struct = 0;
+		while (func_write[index_struct].opw != NULL)
+		{
+			if (*(format + index + 1) == *(func_write[index_struct].opw))
+			{
+			position += (func_write[index_struct].f(valist, (str + position)));
+			}
+		index_struct++;
+		}
+		index = index + 2;
+	}
+	else
+	{
+		*(str +  position) = *(format + index);
+		index++;
+		position++;
+	}
+}
+*(str + position) = '\0';
+return (str);
+}
+
