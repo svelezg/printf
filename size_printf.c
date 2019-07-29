df8214db9eb847daf9529ae87a6912f87598a3fd
svelezg <47928168+svelezg@users.noreply.github.com>
2019-07-29 20:06:34 +0000

    Edge case three %

--- size_printf.c
+++ size_printf.c
@@ -32,7 +32,7 @@ return (size);
  */
 int size_printf(const char * const format, va_list valist)
 {
-int size = 0, index = 0, index_struct = 0;
+int size = 0, index = 0, index_struct = 0, a = 1;
 
 op_s_t func_size[] = {
 	{"c", size_char},
@@ -50,11 +50,14 @@ while (*(format + index))
 			if (*(format + index + 1) == *(func_size[index_struct].ops))
 			{
 			size = (func_size[index_struct].f(valist, size));
+			a = 0;
 			}
 		index_struct++;
 		}
-		size = size_additional(*(format + index +  1), size);
+		if (a = 1)
+			size = size_additional(*(format + index + 1), size);
 		index = index + 2;
+		a = 1;
 	}
 	else
 	{
