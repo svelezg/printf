a29236e3bc6d0261c30930f9bf831906289427be
cavb28 <cavb.28@gmail.com>
2019-07-30 00:53:18 +0000

    task 1 - integer and decimal numbers with style

---
+++ number_printf.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * size_number  - Capitalizes String
+ * @n: string to convert
+ *
+ * Description: String to upper
+ * Upper
+ * Return: char
+*/
+int size_number(int n)
+{
+int cnt = 0;
+
+if (n < 0)
+{
+	cnt++;
+	n *= -1;
+}
+while (n != 0)
+{
+	n = n / 10;
+	cnt++;
+}
+
+return (cnt);
+}
